5c9acd67c52d25b98b7495c5e8727cd22a8d5d2e
muhitesenin <eseninmukhit@gmail.com>
2025-04-18 17:08:39 +0500

    f

--- internal/db/db.go
+++ internal/db/db.go
@@ -14,7 +14,7 @@ import (
 func Connect() *gorm.DB {
 	_ = godotenv.Load()
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
