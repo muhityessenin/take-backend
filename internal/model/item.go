0f3627ea284011f6ea5ec1619565e2cde09a53b6
muhitesenin <eseninmukhit@gmail.com>
2025-04-20 16:49:02 +0500

    wholesael

--- internal/model/item.go
+++ internal/model/item.go
@@ -1,13 +1,14 @@
 package model
 
 type Item struct {
-	ID         uint        `gorm:"primaryKey" json:"id"`
-	Name       string      `json:"name"`
-	PartNumber string      `json:"partNumber"`
-	Brand      string      `json:"brand"`
-	Model      string      `json:"model"`
-	Stock      int         `json:"stock"`
-	Price      int         `json:"price"`
-	Images     []ItemImage `gorm:"foreignKey:ItemID" json:"images"`
-	Sales      []Sale      `gorm:"foreignKey:ItemID"`
+	ID             uint        `gorm:"primaryKey" json:"id"`
+	Name           string      `json:"name"`
+	PartNumber     string      `json:"partNumber"`
+	Brand          string      `json:"brand"`
+	Model          string      `json:"model"`
+	Stock          int         `json:"stock"`
+	Price          int         `json:"price"`
+	WholesalePrice int         `json:"wholesalePrice"`
+	Images         []ItemImage `gorm:"foreignKey:ItemID" json:"images"`
+	Sales          []Sale      `gorm:"foreignKey:ItemID"`
 }
